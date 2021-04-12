192be40bbc66701998c74ebf8b752c0b7d6ef3c2
Otoniel2514 <00045221@uca.edu.sv>
2021-04-12 09:30:24 -0700

    upload

--- Sentencias.cpp
+++ Sentencias.cpp
@@ -9,105 +9,90 @@ int main()
 int edad;
 cout<<"Ingrese su edad"<<endl;
 cin>>edad;
-
-if (edad >18)
+if (edad > 18)
 {
-cout<<"Eres mayor de 18 años"<<endl;
+    cout<<"Eres mayor de 18 anios"<<endl;
 }
 else
 {
-cout<<"Lo sentimos no tienes la edad suficiente"<<endl;
+    cout<<"Lo sentimos no tienes la edad suficiente"<<endl;
 
-return 0;}
+    return 0;
 
 }
 
-//sentencia if */
+    //sentencia if */
 
-//SABER SI UN NUMERO ES MAYOR QUE OTRO(USANDO 3 VARIABLES)
-int A=0, B=0, C=0
-cout<<"Ingrese tres numeros"<<endl;
-cin>>A>>B>>C;
-if (A > B)
-{
-cout<<"A es mayor que B" <<endl;
-}
-else
-{
- cout<<"B es mayor que A" <<endl;
-}
- if (A > C)
-{
-    cout<<"A es mayor que C" <<endl;
-}
-else
-{
-    cout<<"C es mayor que A" <<endl;
-}
-if (C > B)
-{
-    cout<<"C es mayor que B" <<endl;
-}
-else
-{
-    cout<<"B es mayor que C" <<endl;
-}
-//IDENTIFICAR SI UN NUMERO ES PAR O IMPAR 
+ //SABER SI UN NUMERO ES MAYOR QUE OTRO (USANDO 3 VARIABLES)
+    /*int A=0, B=0, C=0;
+    cout<<"Ingrese tres numeros"<<endl;
+    cin>>A>>B>>C;
+    if (A > B)
+    {
+        cout<<"A es mayor que B" <<endl;
+    }
+    else
+    {
+         cout<<"B es mayor que A" <<endl;
+    }
+    if (A > C)
+    {
+        cout<<"A es mayor que C" <<endl;
+    }
+    else
+    {
+         cout<<"C es mayor que A" <<endl;
+    }
+    if (C > B)
+    {
+        cout<<"C es mayor que B" <<endl;
+    }
+    else
+    {
+         cout<<"B es mayor que C" <<endl;
+    }*/
+//IDENTIFICAR SI UN NUMERO ES PAR O IMPAR
+/*
 int x;
 cout<<"Ingrese su numero"<<endl;
 cin>>x;
-
 if (x>0)
 {
-    cout<<"El numero es natural"<<endl;    
+    cout<<"El numero es natural"<<endl;
 }
-
 else if (x%2==0)
 {
     cout<<"El numero es par"<<endl;
-
 }
 else
 {
-    cout<<"El numero es impar"<<endl;
-
-    return 0;}
-}
+    cout<<"Numero es impar"<<endl;
+} 
 */
 //IMPLEMENTACION DE LA SENTENCIA SWITCH
-/ int num;
+/*int num;
 cout<<"Ingrese un numero entre 1 y 5"<<endl;
 cin>>num;
-
 switch (num)
 {
-case 1:
-cout<<"Este es el primer caso"<<endl;
+case 1: 
+cout<<"Este es el primer caso"<<endl;   
 break;
-
-case 2:
-cout<<"Este es el segundo caso"<<endl;
+case 2:  
+cout<<"Este es el segundo caso"<<endl;  
 break;
-
-case 3:
-cout<<"Este es el tercer caso"<<endl;
+case 3: 
+cout<<"Este es el tercer caso"<<endl;   
 break;
-
-case 4:
+case 4:    
 cout<<"Este es el cuarto caso"<<endl;
 break;
-
-case 5:
+case 5:    
 cout<<"Este es el quinto caso"<<endl;
 break;
-
 default:
-    break;
-    
-    return 0;}
-}
-
-*/
+    break;   
+}*/
 //SENTENCIA SWITCH VOCALES
 char vocal;
 cout<<"Ingrese su vocal"<<endl;

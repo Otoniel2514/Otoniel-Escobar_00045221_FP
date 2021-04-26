d805d58dcc66f30344c3d84a5ea1ec0d50f4fc36
Otoniel2514 <00045221@uca.edu.sv>
2021-04-26 13:34:32 -0700

    Upload

--- vocales.cpp
+++ vocales.cpp
@@ -1,21 +1,28 @@
+//Crear un programa que nos permita identificar dentro del abecedario español castellano cuales son vocales y cuales no. si es vocal o no 
+
 #include "iostream"
 using namespace std;
-int main(){
+int main()
+{
     //Declaracion de variable a utilizar en el programa
     char letra;
-
-    cout<<"1. Digite un caracter alfabetico: ";
-    cout<<"2. Salir y digitar la siguiente letra: ";
-    cin>>letra;
-
-    switch (letra){
-    case 'a':
-    case 'e':
-    case 'i':
-    case 'o':
-    case 'u': cout<<"Su caracter alfabetico digitado si es una vocal";break;
-    default: cout<<"Su caracter alfabetico digitado no es una vocal";break;
+    { 
+        cout<<"\nDigite su letra del alfabeto"<<endl;
+        cin>>letra;    
+            
     }
-    return 0;
+        if(letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u')
+        cout<<"Su letra del alfabeto ingresada si es una vocal."<<endl;
+        
+        else
+        cout<<"\nSu letra del alfabeto ingresada no es una vocal, intentelo de nuevo por favor."<<endl;
+        
+
+       return 0;
 }
+
+
+
+
+
 

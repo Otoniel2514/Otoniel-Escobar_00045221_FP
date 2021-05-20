492d589bb80f428369dec9860c2466328684fd80
Otoniel2514 <00045221@uca.edu.sv>
2021-05-19 23:21:24 -0700

    Upload

---
+++ Tienda.cpp
@@ -0,0 +1,101 @@
+//Realizar un programa que sea una tienda donde podamos almacenar productos y precios y mostrar una factura al cliente
+
+#include "iostream"
+using namespace std;
+const int longCade=20;
+
+struct
+{
+    char nombreArticulo [longCade + 1][50];
+    int cantidad[50];
+    float precio[50];
+    float costoArticulo[50];
+} tienda;
+
+void informacion(int nArticulos)
+{
+    cout<<"\tInformacion de los productos"<<endl;
+    for (int i = 0; i < nArticulos; i++)
+    {
+        cin.ignore();
+        cout<<"Ingrese el nombre del Articulo "<< i+1 <<": "<<endl;
+        cin>>tienda.nombreArticulo[i];
+        cout<<"Ingrese la cantidad de Articulos que desea: "<<endl;
+        cin>>tienda.cantidad[i];
+        cout<<"Ingrese el precio por unidad de su articulo: "<<endl;
+        cin>>tienda.costoArticulo[i];
+    }
+    return;
+}
+void precios(int nArticulos)
+{
+    for (int i = 0; i < nArticulos; i++)
+    {
+        tienda.precio[i] = tienda.costoArticulo[i] * tienda.cantidad[i];
+    }
+    return;
+}
+void subtotal(int nArticulos)
+{
+    cout<<"\t Resultados de los productos que se esta pidiendo: "<<endl;
+    for (int i = 0; i < nArticulos; i++)
+    {
+        cout<<"Producto: "<<tienda.nombreArticulo[i]<<endl;
+        cout<<"Cantidad: "<<tienda.cantidad[i]<<endl;
+        cout<<"Costo por el articulo: $"<<tienda.precio[i]<<endl;;
+    }
+    return;
+}
+void total(int nArticulos)
+{
+    cout<<"El total de tu cuenta es: "<<endl;
+    float total;
+    float iva;
+    for (int i = 0; i < nArticulos; i++)
+    {
+       total += tienda.precio[i];
+    }
+    cout<<"\tTotal sin iva....es $"<<total<<endl;
+    iva = total * 0.13;
+    cout<<"\nIva....$"<<iva<<endl;
+    cout<<"\nTotal de su cuenta....$"<<total+iva<<endl;
+}
+void program()
+{
+    int nArticulos = 0;
+    cout<<"Cantidad de articulos (50 maximo)"<<endl;
+    cin>>nArticulos;
+    informacion(nArticulos);
+    precios(nArticulos);
+    subtotal(nArticulos);
+    total(nArticulos);
+}
+int main()
+{
+program();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
